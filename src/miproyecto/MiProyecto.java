b240ca4756d883df27c5a9f74890509e81aa1d2d
rjimenez <rjimenez@RJ>
2016-03-13 19:59:20 -0600

    Primer comit

--- src/miproyecto/MiProyecto.java
+++ src/miproyecto/MiProyecto.java
@@ -16,7 +16,8 @@ public class MiProyecto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        // HOla
+        // HOLA
+        // ADIOS
     }
     
 }
